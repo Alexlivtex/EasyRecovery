26028d72ab7675fd14a1ae7232ec65591ce3bfcb
Alex Lee <525210804@qq.com>
2018-01-31 19:49:35 +0800

    Update backup.py
    Added upload operation

--- backup.py
+++ backup.py
@@ -1,24 +1,49 @@
 import tarfile
 import shutil
 import os
-import sys
+import signal
+from functools import wraps
+import errno
+from bypy import ByPy
+import time
 
 BACKUP_FOLDER = "BackupTarget"
+BACKUP_FILE = "BackupData"
 BASE_PATH = os.path.abspath(os.pardir)
 
-#tar = tarfile.open("Result.tar.gz", "w:gz")
-#tar.add("/root/test_compress", "Result")
-#tar.close()
-
-#shutil.copytree("/root/test_copy/target", "/root/test_copy/test/target")
+MAX_UPLOAD_DOWNLOAD = 15*60
+MAX_TIME_UPLOAD_SLEEP = 5*60
 
 module_list = ["ResourceDownloader"]
 
+class TimeoutError(Exception):
+    pass
+
+def timeout(seconds=10, error_message=os.strerror(errno.ETIME)):
+    def decorator(func):
+        def _handle_timeout(signum, frame):
+            raise TimeoutError(error_message)
+
+        def wrapper(*args, **kwargs):
+            signal.signal(signal.SIGALRM, _handle_timeout)
+            signal.alarm(seconds)
+            try:
+                result = func(*args, **kwargs)
+            finally:
+                signal.alarm(0)
+            return result
+
+        return wraps(func)(wrapper)
+
+    return decorator
 
 def backup_begin():
     #1:Copy file_target
     if os.path.exists(BACKUP_FOLDER):
         shutil.rmtree(BACKUP_FOLDER)
+
+    if os.path.exists(BACKUP_FILE + ".tar.gz"):
+        os.remove(BACKUP_FILE + ".tar.gz")
 
     os.mkdir(BACKUP_FOLDER)
 
@@ -30,5 +55,25 @@ def backup_begin():
 
     shutil.copytree(os.path.join(BASE_PATH, "System"), os.path.join(BACKUP_FOLDER, "System"))
     shutil.copyfile("/etc/shadowsocks.json", os.path.join(BACKUP_FOLDER, "System", "shadowsocks.json"))
+
+    tar = tarfile.open(BACKUP_FILE + ".tar.gz", "w:gz")
+    tar.add(BACKUP_FOLDER, BACKUP_FILE)
+    tar.close()
+
+    ##### 2nd Download the download the bt video
+    @timeout(MAX_UPLOAD_DOWNLOAD)
+    def upload_data(path):
+        bp = ByPy()
+        bp.upload(path)
+        bp.cleancache()
+
+    while True:
+        try:
+            upload_data(BACKUP_FILE + ".tar.gz")
+            break
+        except:
+            print("Uupload failed, try again!")
+            time.sleep(MAX_TIME_UPLOAD_SLEEP)
+            continue
 
 backup_begin()
