d21de90ff631e4dad57dca5705bd7beb44460725
Wei Li <525210804@qq.com>
2018-02-20 22:20:41 +0800

    Added code to support wordpress and system config

---
+++ sys_recover.py
@@ -0,0 +1,52 @@
+import json
+import socket
+import os
+
+SHADOWSOCKS_CONFIG = "/etc/shadowsocks.json"
+
+def get_host_ip():
+   try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      s.connect(('8.8.8.8', 80))
+      ip = s.getsockname()[0]
+   finally:
+      s.close()
+   return ip
+
+
+
+def config_system():
+    address = get_host_ip()
+
+    data = {}
+
+    print("Please input the password for the ss server : ")
+    password = input()
+
+    data["server"] = str(address)
+    data["port_password"] = {}
+    data["port_password"]["8381"] = password
+    data["port_password"]["8382"] = password
+    data["port_password"]["8383"] = password
+    data["port_password"]["8386"] = password
+    data["port_password"]["8388"] = password
+
+    data["timeout"] = 600
+    data["method"] = "aes-256-cfb"
+    data["fast_open"] = "false"
+
+    f = open(SHADOWSOCKS_CONFIG, "w")
+    f.write(json.dumps(data, ensure_ascii=False, indent=4, separators=(",", ":")))
+    f.close()
+
+    os.system("service shadowsocks start")
+    os.system("ssserver -c " + SHADOWSOCKS_CONFIG + " -d start")
+
+    os.system("wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh")
+    os.system("chmod +x bbr.sh")
+    os.system("./bbr.sh")
+
+
+config_system()
+
+
---
+++ wp_setup.py
@@ -0,0 +1,44 @@
+import os
+
+def wp_recover():
+    apache2_packages = ["apache2", "apache2-utils"]
+    os.system("apt-get install " + " ".join(apache2_packages))
+
+    os.system('sed -i "s/Options Indexes FollowSymLinks/Options FollowSymLinks/" /etc/apache2/apache2.conf')
+
+    os.system("systemctl enable apache2")
+    os.system("systemctl start apache2")
+
+    mysql_packages = ["mysql-client", "mysql-server"]
+    os.system("apt-get install " + " ".join(mysql_packages))
+
+    os.system("mysql_secure_installation")
+
+    php_packages = ["php", "php-mysql", "libapache2-mod-php", "php-cli", "php-cgi", "php-gd", "php-curl"]
+    os.system("pt-get install " + " ".join(php_packages))
+
+    os.system("wget -c http://wordpress.org/latest.tar.gz")
+    os.system("tar -xzvf latest.tar.gz")
+
+    os.system("rsync -av wordpress/* /var/www/html/")
+    os.system("chown -R www-data:www-data /var/www/html/")
+
+    os.system("chmod -R 755 /var/www/html/")
+
+    os.system("mysql -u root -p")
+
+    #mysql > CREATE DATABASE wp_myblog;
+    #mysql > GRANT ALL PRIVILEGES ON wp_myblog. * TO 'your_username_here' @ 'localhost' IDENTIFIED BY 'your_chosen_password_here';
+    #mysql > FLUSH PRIVILEGES;
+    #mysql > EXIT;
+
+    #systemctl restart apache2.service
+    #systemctl restart mysql.service
+
+wp_recover()
+
+
+
+
+
+
