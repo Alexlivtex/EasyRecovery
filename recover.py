805d558577cd8464b87d3abb149bdf3fddf3ce55
Wei Li <525210804@qq.com>
2018-03-03 23:17:27 +0800

    Updated recovery and deleted wordpres related operation

--- recover.py
+++ recover.py
@@ -11,7 +11,7 @@ def recover():
         os.system(clone_str)
 
     os.system("wget https://chromedriver.storage.googleapis.com/2.35/chromedriver_linux64.zip")
-    os.system("wget http://wordpress.org/latest.tar.gz")
+    #os.system("wget http://wordpress.org/latest.tar.gz")
     if not os.path.exists("ta-lib-0.4.0-src.tar.gz"):
         os.system("wget https://nchc.dl.sourceforge.net/project/ta-lib/ta-lib/0.4.0/ta-lib-0.4.0-src.tar.gz")
 
@@ -38,7 +38,7 @@ def recover():
     os.system("wget -q -O - https://dl.google.com/linux/linux_signing_key.pub  | sudo apt-key add -")
     os.system("apt-get update")
 
-    ubuntu_packages = ["python-pip", "python3-pip", "python-libtorrent", "python3-libtorrent", "google-chrome-stable", "xvfb", "shadowsocks", "vsftpd"]
+    ubuntu_packages = ["python-pip", "python3-pip", "python-libtorrent", "python3-libtorrent", "google-chrome-stable", "xvfb", "vsftpd"]
 
     for apt_index in ubuntu_packages:
         os.system("apt-get --assume-yes install " + apt_index)
@@ -51,19 +51,19 @@ def recover():
         os.system(pip2_str)
         os.system(pip3_str)
 
-    os.system("touch startup.sh")
+    os.system("touch /root/startup.sh")
     os.system("chmod 777 startup.sh")
     with open("startup.sh", "a+") as f_startup:
         f_startup.write("Xvfb :99 -ac &\n")
         f_startup.write("export DISPLAY=:99\n")
-        f_startup.write("service shadowsocks start\n")
-        f_startup.write("ssserver -c /etc/shadowsocks.json -d start\n")
+        #f_startup.write("service shadowsocks start\n")
+        #f_startup.write("ssserver -c /etc/shadowsocks.json -d start\n")
 
     with open("/root/.bashrc", "a+") as f_bashrc:
         f_bashrc.write("export DISPLAY=:99\n")
 
-    tar = tarfile.open("BackupData.tar.gz")
-    tar.extractall()
-    tar.close()
+    #tar = tarfile.open("BackupData.tar.gz")
+    #tar.extractall()
+    #tar.close()
 
 recover()
